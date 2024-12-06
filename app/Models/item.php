6cd0167c6097bfe77850ba56d9601e9b3befd4b2
Jhonatan <jhonatann36@gmail.com>
2024-12-06 16:23:02 -0300

    feat: add models

---
+++ app/Models/item.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class item extends Model
+{
+    protected $table = 'item';
+
+    
+}
---
+++ app/Models/notaFiscal.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class notaFiscal extends Model
+{
+    protected $table = 'notaFiscal';
+
+    protected $primaryKey = 'idNota';
+
+    
+}
